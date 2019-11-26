0b30f66c9ba809036ab2a5e43c7158781bf7e7a2
varun27896 <varun27896@gmail.com>
2019-11-26 17:50:04 -0600

    Added index.ejs file and INDEX route that uses GET method

--- RESTfulBlogApp/app.js
+++ RESTfulBlogApp/app.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:2701/restful_blog_app", {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect("mongodb://localhost/restful_blog_app", {useUnifiedTopology: true, useNewUrlParser: true});
 
 
 var blogSchema = new mongoose.Schema({
@@ -12,10 +12,23 @@ var blogSchema = new mongoose.Schema({
     body: String,
     created: {type: Date, default: Date.now}
 });
-
 var Blog = mongoose.model("Blog", blogSchema);
 
 
+
+
+app.get("/blogs", function(req, res){
+    Blog.find({}, function (err, blogs){
+        if(err){
+            console.log("errrr");
+        }else{
+            res.render("index", {blogs:blogs});
+
+        }
+    
+        
+    });
+})
 
 
 var port = process.env.PORT || 3000;
@@ -24,6 +37,7 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
 
 
 
